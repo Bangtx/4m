d9d6ba9e86071b4be3516196a1c79246f8f162ee
banh <bangtx@codenet.jp>
2023-08-10 08:53:35 +0700

    add init java backend

--- api/src/main/java/m/com/vn/api/Member.java
+++ api/src/main/java/m/com/vn/api/Member.java
@@ -4,16 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
 @RestController
-public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+@RequestMapping('/member')
+public class Member {
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
