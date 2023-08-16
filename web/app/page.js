3195eda3a04b31db51b1b1a4d5f26d14a1bb6726
banh <bangtx@codenet.jp>
2023-08-16 21:52:06 +0700

    config cors

--- web/app/page.js
+++ web/app/page.js
@@ -3,6 +3,8 @@ import styles from "@/app/page.module.css";
 import {HeaderBar, CommonTable, SearchAndAdd, CompanyDialog} from "@/app/component";
 import {useState} from "react";
 import {Typography, Box, TextField, Button, Grid, Hidden, Stack} from "@mui/material";
+import {render} from "react-dom";
+import api from '@/app/plugins/api'
 
 
 const columns = [
@@ -13,11 +15,15 @@ const columns = [
 ]
 
 export default function Home() {
+    const [companies, setCompanies] = useState([])
+
+    const {data} = api.get('http://localhost:1001/company/')
+
     const [showCompanyDialog, setShowCompanyDialog] = useState(false)
-    const companies = [
-        {id: 1, code: 1, name: 'comtect', address: 'Ha Noi'},
-        {id: 2, code: 2, name: '4m company'},
-    ]
+    // const companies = [
+    //     {id: 1, code: 1, name: 'comtect', address: 'Ha Noi'},
+    //     {id: 2, code: 2, name: '4m company'},
+    // ]
     return (
         <main className={styles.main}>
             <HeaderBar/>
